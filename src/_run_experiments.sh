98dcfed54670d4d3c1775e97d60e2a78ff9862e1
zyyjjj <yz685@cornell.edu>
2025-03-30 22:34:01 -0400

    update experiment-running shell script

--- src/_run_experiments.sh
+++ src/_run_experiments.sh
@@ -5,5 +5,6 @@ do
     python run_meta_simulation_g2.py \
         --seed $seed --init_prev 0.01 --pool_size 10 --LoD 1240 --edge_weight 10 \
         --num_groups 5 --alpha 2 --peak_VL 6 --distancing_scale 50.0 \
-        --dilute average --beta 0.1 --pooling_method naive correlated &
+        --dilute average --beta 0.1 \
+        --pooling_method naive correlated correlated_weak &
 done
